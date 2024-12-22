fe79b37cc77fd8377431ebf41ed9d86c4c40a539
isnastish <paukden12@gmail.com>
2024-12-22 13:37:26 +0100

    added auth test

--- pkg/auth/auth_test.go
+++ pkg/auth/auth_test.go
@@ -7,12 +7,13 @@ import (
 )
 
 const secret = "my-secret-secret"
+const email = "admin@gmail.com"
 
 func TestJwtTokenExpired(t *testing.T) {
 	accessTokenTTL := time.Minute * 1
 	m := NewAuthManager([]byte(secret), accessTokenTTL)
 
-	tokenPair, err := m.GetTokenPair("admin@gmail.com")
+	tokenPair, err := m.GetTokenPair(email)
 	if err != nil {
 		t.Errorf("failed to get token pair %v", err)
 	}
@@ -27,5 +28,17 @@ func TestJwtTokenExpired(t *testing.T) {
 }
 
 func TestSuccessfullAccessTokenValidation(t *testing.T) {
+	m := NewAuthManager([]byte(secret), time.Second*30) // 30 seconds TTL
 
+	tokenPair, err := m.GetTokenPair(email)
+	if err != nil {
+		t.Errorf("failed to get token pair %v", err)
+	}
+
+	// wait 15 seconds
+	time.Sleep(time.Second * 15)
+
+	if err := m.ValidateJwtToken(tokenPair.AccessToken); err != nil {
+		t.Error(err)
+	}
 }
