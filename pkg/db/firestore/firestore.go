d514530d53178a2d9049c1ba1b3b9a7d2eb2ad36
isnastish <paukden12@gmail.com>
2025-01-08 18:04:34 +0100

    rewrote firebase backend

--- pkg/db/firestore/firestore.go
+++ pkg/db/firestore/firestore.go
@@ -1,59 +1,57 @@
-package firebase
+package firestore
 
 import (
 	"context"
 	"fmt"
 	"os"
 
-	firebase "firebase.google.com/go/v4"
-	"firebase.google.com/go/v4/db"
-	"google.golang.org/api/option"
+	"cloud.google.com/go/firestore"
 
 	"github.com/isnastish/openai/pkg/api/models"
 )
 
+// Cloud Firestore stores data in Documents,
+// which are stored in Collections.
+// Cloud Firestore creates collections and documents implicitly the first time you add data to the document. You do not need to explicitly create collections or documents.
+
+// NOTE: Context should never be a part of a struct,
+// but instead passed to each function that needs it.
 type FirestoreController struct {
-	// firestore database client
-	dbClient *db.Client
+	client *firestore.Client
 }
 
+func (db *FirestoreController) Close() error {
+	return nil
+}
+
+// TODO: Passing context to a constructor might not be the best solution.
+// Since it doesn't contain any deadlines nor cancelations.
 func NewFirestoreController(ctx context.Context) (*FirestoreController, error) {
-	// TODO: Read about refresh-token credentials file
-	opt := option.WithCredentialsFile("path/to/refreshToken.json")
-	config := &firebase.Config{ProjectID: "my-project-id"}
+	var client *firestore.Client
+	var err error
 
-	app, err := firebase.NewApp(ctx, config, opt)
+	projectId, set := os.LookupEnv("FIRESTORE_PROJECT_ID")
+	if !set || projectId == "" {
+		projectId = firestore.DetectProjectID
+	}
+
+	databaseId, set := os.LookupEnv("FIRESTORE_DATABASE_ID")
+	if set { // the variable is set, but the value might be empty
+		client, err = firestore.NewClientWithDatabase(ctx, projectId, databaseId)
+	} else {
+		client, err = firestore.NewClient(ctx, projectId)
+	}
+
 	if err != nil {
-		return nil, fmt.Errorf("firebase: failed to create firebase app, error: %v", err)
+		return nil, fmt.Errorf("firesbase: failed to create client: %v", err)
 	}
-
-	firestoreDbUrl, set := os.LookupEnv("FIRESTORE_URL")
-	if !set || firestoreDbUrl == "" {
-		return nil, fmt.Errorf("firebase: FIRESTORE_URL is not set")
-	}
-
-	client, err := app.DatabaseWithURL(ctx, firestoreDbUrl)
-	if err != nil {
-		return nil, fmt.Errorf("firebase: failed to initialize database client, error: %v", err)
-	}
-
-	// usersRef := client.NewRef("/users")
 
 	return &FirestoreController{
-		dbClient: client,
+		client: client,
 	}, nil
 }
 
 func (db *FirestoreController) AddUser(ctx context.Context, userData *models.UserData, geolocationData *models.GeolocationData) error {
-	// NOTE: This is a rough approximation of how this supposed to
-	usersRef := db.dbClient.NewRef("/users")
-	newUserRef, err := usersRef.Push(ctx, nil)
-	if err != nil {
-		return fmt.Errorf("firestore: failed to create user ref, error: %v", err)
-	}
-
-	_ = newUserRef
-
 	return nil
 }
 
@@ -63,8 +61,4 @@ func (db *FirestoreController) GetUserByEmail(ctx context.Context, email string)
 
 func (db *FirestoreController) GetUserByID(ctx context.Context, id int) (*models.UserData, error) {
 	return nil, nil
-}
-
-func (db *FirestoreController) Close() error {
-	return nil
 }
