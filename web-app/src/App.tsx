43b3ec5c207b21b91ed8249afaf707457aad5a15
isnastish <paukden12@gmail.com>
2025-03-08 16:38:15 +0100

    using react router

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
 
+import { BrowserRouter, Routes, Route } from 'react-router';
+
 function App() {
     return (
-        <div className="container">
-          <div className='row justify-content-center mt-5'>
-            <div className='col-md-5'>
-                <h3 className="text-center">Login</h3>
-                <div className="input-group flex-nowrap">
-                  <span className="input-group-text" id="addon-wrapping">email</span>
-                  <input type="text" autoFocus={true} required className="form-control" placeholder="admin@gmail.com" aria-label="Username" aria-describedby="addon-wrapping"/>
-                </div>
-                <hr></hr>
-                <div className="col-12">
-                  <button type="submit" className="btn btn-outline-primary">Sign in</button>
-                </div>
-
-            </div>
-          </div>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/login"
+                    element={
+                        <div className="container">
+                            <div className="row justify-content-center mt-5">
+                                <div className="col-md-5">
+                                    <h3 className="text-center">Login</h3>
+                                    <div className="input-group flex-nowrap">
+                                        <span
+                                            className="input-group-text"
+                                            id="addon-wrapping"
+                                        >
+                                            email
+                                        </span>
+                                        <input
+                                            type="text"
+                                            autoFocus={true}
+                                            required
+                                            className="form-control"
+                                            placeholder="admin@gmail.com"
+                                            aria-label="Username"
+                                            aria-describedby="addon-wrapping"
+                                        />
+                                    </div>
+                                    <hr></hr>
+                                    <div className="col-12">
+                                        <button
+                                            type="submit"
+                                            className="btn btn-outline-primary"
+                                        >
+                                            Sign in
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    }
+                ></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
