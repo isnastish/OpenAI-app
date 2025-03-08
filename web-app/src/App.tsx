c09b6d411b7315d8e3419f5c05d7ee94bcafc989
isnastish <paukden12@gmail.com>
2025-03-08 19:05:54 +0100

    implemented ai route

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,7 +1,13 @@
 import React, { FormEvent } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router';
 
 const LoginView: React.FC = () => {
+    const navigate = useNavigate();
+
+    const onSubmitLoginData = (): void => {
+        navigate('/ai');
+    };
+
     return (
         <div className="container">
             <div className="row justify-content-center mt-5">
@@ -28,9 +34,33 @@ const LoginView: React.FC = () => {
                         <button
                             type="submit"
                             className="btn btn-outline-primary"
+                            onClick={onSubmitLoginData}
                         >
                             Sign in
                         </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const AiView: React.FC = () => {
+    return (
+        <div className="container">
+            <div className="row justify-content-center mt-5">
+                <div className="col-md-7">
+                    <h3 className="text-center">
+                        <p className="font-monospace">Ask AI</p>
+                    </h3>
+                    <div className="mb-3">
+                        <textarea
+                            className="form-control rounded-4"
+                            id="floatingTextarea2"
+                            placeholder="Ask any question"
+                            rows={5}
+                            autoFocus={true}
+                        ></textarea>
                     </div>
                 </div>
             </div>
@@ -44,6 +74,7 @@ function App() {
             <Routes>
                 <Route path="/login" element={<LoginView />}></Route>
                 <Route path="/" element={<LoginView />}></Route>
+                <Route path="/ai" element={<AiView />}></Route>
             </Routes>
         </BrowserRouter>
     );
